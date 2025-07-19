701e7c1fb0e226d678dfe5d535e0d1331201e87a
imshivam2002 <imshivam97555@gmail.com>
2025-07-19 14:46:39 +0530

    first commit

--- frontend/src/data/DoctorData.js
+++ frontend/src/data/DoctorData.js
@@ -1,40 +1,42 @@
 const doctors = [
   {
-    name: "Dr. Ayesha Khan",
+    name: "Dr. Gunjan",
     specialty: "Nutritionist",
     contact: "123-456-7890",
-    image: "https://randomuser.me/api/portraits/women/44.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp-iBNS6Gs4SXMaGncb6xo5LdIUBRcU65Nsw&s",
   },
   {
-    name: "Dr. Rajeev Verma",
+    name: "Dr. Priya",
     specialty: "Cardiologist",
     contact: "987-654-3210",
-    image: "https://randomuser.me/api/portraits/men/32.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT38doy6ByEPedExrBL3WfP667Nj9tbUEj78g&s",
   },
   {
-    name: "Dr. Neha Sharma",
+    name: "Dr. Zoya",
     specialty: "Endocrinologist",
     contact: "555-123-9999",
-    image: "https://randomuser.me/api/portraits/women/65.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLKpFBpPBmG4kzzbJLnlfk08C-lV1T90XesQ&s",
   },
   {
-    name: "Dr. Aarti Sharma",
+    name: "Dr. Sonali",
     specialty: "Nutritionist",
     contact: "9876543210",
-    image: "https://randomuser.me/api/portraits/women/68.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSquwYtZ0g2VPNHSJNh2C1xo_3bFIme0e-AlsMUam9L5WbJeK8FQPZJJoflenZFn9yFmYU&usqp=CAU",
   },
   {
-    name: "Dr. Aarti Sharma",
+    name: "Dr. jiya",
     specialty: "Nutritionist",
     contact: "9876543210",
-    image: "https://randomuser.me/api/portraits/women/68.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqceR5WxFRezMYh6Alz0sod9dFyPt3eyCynw&s",
   },
   {
-    name: "Dr. Aarti Sharma",
+    name: "Dr. Yash",
     specialty: "Nutritionist",
     contact: "9876543210",
-    image: "https://randomuser.me/api/portraits/women/68.jpg",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHMBhFxj8ALALNfmTGFLmxsnHwuAZU9raQYQ&s",
   },
 ];
-
 export default doctors;
+
+
+
